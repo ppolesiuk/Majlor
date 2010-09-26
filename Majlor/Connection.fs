6da3bf00ee4417482baf7cf885d1268473988277
poles-p <poles_p@wp.pl>
2010-09-26 18:20:06 +0200

    Start

---
+++ Majlor/Connection.fs
@@ -0,0 +1,11 @@
+﻿namespace Majlor.SMTP
+
+open System.IO
+open System.Net.Sockets
+
+type Connection(stream : NetworkStream) =
+    let writer = new StreamWriter(stream)
+    do
+        writer.WriteLine "421 Service not available."
+        writer.Close()
+        stream.Close()
---
+++ Majlor/Program.fs
@@ -0,0 +1,16 @@
+﻿module Majlor.Program
+
+open System
+open System.Net
+open System.Net.Sockets
+
+let smtpServe() =
+    let server = TcpListener(IPAddress.Any, 587)
+    server.Start()
+    while true do
+        let client = server.AcceptTcpClient();
+        let connection = Majlor.SMTP.Connection(client.GetStream())
+        ()
+
+let main =
+    smtpServe()
